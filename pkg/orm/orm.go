79cd7c37d64b8069256ceda1050956d905ad700e
fusidic <arithbar@gmail.com>
2021-01-25 12:25:30 +0800

    feature: commit

--- pkg/orm/orm.go
+++ pkg/orm/orm.go
@@ -2,6 +2,8 @@ package orm
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/fusidic/orm/pkg/dialect"
 	"github.com/fusidic/orm/pkg/log"
@@ -79,4 +81,59 @@ func (e *Engine) Transaction(f TxFunc) (result interface{}, err error) {
 		}
 	}()
 	return f(s)
+}
+
+// difference returns a - b
+func difference(a []string, b []string) (diff []string) {
+	mapB := make(map[string]bool)
+	for _, v := range b {
+		mapB[v] = true
+	}
+	for _, v := range a {
+		if _, ok := mapB[v]; !ok {
+			diff = append(diff, v)
+		}
+	}
+	return
+}
+
+// Migrate table
+func (e *Engine) Migrate(value interface{}) error {
+	_, err := e.Transaction(func(s *session.Session) (result interface{}, err error) {
+		if !s.Model(value).HasTable() {
+			log.Infof("table %s doesn't exist", s.GetRefTable().Name)
+			return nil, s.CreateTable()
+		}
+		// schema we set
+		table := s.GetRefTable()
+		// schema in database
+		rows, _ := s.Raw(fmt.Sprintf("SELECT * FROM %s LIMIT 1", table.Name)).QueryRows()
+		columns, _ := rows.Columns()
+		addCols := difference(table.FieldNames, columns)
+		delCols := difference(columns, table.FieldNames)
+		log.Infof("added cols %v, deleted cols %v", addCols, delCols)
+
+		// Loop: add column to table
+		for _, col := range addCols {
+			f := table.GetField(col)
+			sqlStr := fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s;", table.Name, f.Name, f.Type)
+			if _, err = s.Raw(sqlStr).Exec(); err != nil {
+				return
+			}
+		}
+
+		if len(delCols) == 0 {
+			return
+		}
+
+		// Migrate
+		tmp := "tmp_" + table.Name
+		fieldStr := strings.Join(table.FieldNames, ", ")
+		s.Raw(fmt.Sprintf("CREATE TABLE %s AS SELECT %s from %s;", tmp, fieldStr, table.Name))
+		s.Raw(fmt.Sprintf("DROP TABLE %s;", table.Name))
+		s.Raw(fmt.Sprintf("ALTER TABLE %s RENAME TO %s", tmp, table.Name))
+		_, err = s.Exec()
+		return nil, err
+	})
+	return err
 }
--- pkg/orm/orm_test.go
+++ pkg/orm/orm_test.go
@@ -2,6 +2,7 @@ package orm
 
 import (
 	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/fusidic/orm/pkg/session"
@@ -65,5 +66,22 @@ func testTransactionCommit(t *testing.T) {
 	_ = s.First(u)
 	if err != nil || u.Name != "Tom" {
 		t.Fatal("failed to commit")
+	}
+}
+
+// test Migrate
+func Test_Engine_Migrate(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXISTS User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text PRIMARY KEY, XXX integer);").Exec()
+	_, _ = s.Raw("INSERT INTO User(`Name`) values(?), (?)", "Tom", "Sam").Exec()
+	engine.Migrate(&User{})
+
+	rows, _ := s.Raw("SELECT * FROM User").QueryRows()
+	columns, _ := rows.Columns()
+	if !reflect.DeepEqual(columns, []string{"Name", "Age"}) {
+		t.Fatal("Failed to migrate table User, got columns", columns)
 	}
 }
